54f400a411f3ef1db7c86147dc5fd557a92b9977
Hossein Krouna <krouna.hossein@gmx.net>
2022-12-28 20:31:26 +0100

    add Userstory #0

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -5,37 +5,82 @@ const colors = [
   {
     id: "1",
     colorCode: "#ccc",
+    name: "gray80",
   },
   {
     id: "2",
     colorCode: "#54FF9F",
+    name: "SeaGreen1",
   },
   {
     id: "3",
     colorCode: "#ee1289",
+    name: "DeepPink2",
   },
   {
     id: "4",
     colorCode: "#FFFF00",
+    name: "yellow",
   },
 ];
 
-export default function ColorCard(id, colorCode) {
+export default function ColorCard() {
   return (
     <StyledList>
-      {colors.map(({ id, colorCode }) => (
-        <ListItem style={{ backgroundColor: colorCode }} key={id}>
-          {colorCode}
-        </ListItem>
+      {colors.map(({ id, colorCode, name }) => (
+        <CardStyle>
+          <CardItem
+            onClick={() => {
+              navigator.clipboard.writeText(colorCode);
+            }}
+            style={{ backgroundColor: colorCode }}
+            key={id}
+            name={name}
+            colorCode={colorCode}
+          >
+            <Colorname>{name}</Colorname>
+            <DescriptionStyle>{colorCode}</DescriptionStyle>
+          </CardItem>
+        </CardStyle>
       ))}
     </StyledList>
   );
 }
 
-const ListItem = styled.li`
+//TODO - position Colorname seperate from description
+const Colorname = styled.p`
+  font-size: large;
+  font-weight: 700;
+`;
+
+const CardItem = styled.div`
+  height: 300px;
+  width: 200px;
   border: 1px solid black;
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  align-self: center;
+  justify-content: center;
 `;
 
 const StyledList = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
   list-style-type: none;
+`;
+
+const CardStyle = styled.section`
+  cursor: pointer;
+  display: flex;
+  padding: 5px;
+`;
+//TODO - auto sizing depended of div-element
+const DescriptionStyle = styled.p`
+  padding: 10px;
+  margin: 0;
+  border-radius: 10px;
+  background-color: white;
+  border: 1px solid black;
+  font-weight: bold;
 `;
